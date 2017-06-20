bce51605ae13a3276eb4c4a04c113d224fd86b8c
Joshua Fairchild and guy anderson <joshua+guy@>
2017-06-20 11:31:00 -0700

    pass test for Post.GetUsersWhoDislike method

--- Tests/PostTests.cs
+++ Tests/PostTests.cs
@@ -118,6 +118,26 @@ namespace SocialMedia.Objects
       Assert.Equal(controlList, testList);
     }
 
+    [Fact]
+    public void Post_GetUsersWhoDislike_ReturnsListOfUsers()
+    {
+      Post newPost = new Post("Hello world", 1, new DateTime(2017, 06, 19));
+      newPost.Save();
+
+      User user1 = new User("Joshua", "Fairchild", "jfairchild", "password", "mail@mail.com", new DateTime(2017, 06, 19));
+      user1.Save();
+      User user2 = new User("Guy", "Anderson", "ganderson", "password", "mail@mail.com", new DateTime(2017, 06, 19));
+      user2.Save();
+
+      user1.DislikePost(newPost);
+      user2.DislikePost(newPost);
+
+      List<User> testList = newPost.GetUsersWhoDislike();
+      List<User> controlList = new List<User>{user1, user2};
+
+      Assert.Equal(controlList, testList);
+    }
+
     public void Dispose()
     {
       Post.DeleteAll();

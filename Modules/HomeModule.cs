f0a258c1501051e2bce2e6a2356fdcbbc7cf2916
Joshua Fairchild <Joshua.fairchild429@gmail>
2017-06-20 18:32:31 -0700

    add Post route allowing the user to enter a post into the form on news.cshtml

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -13,7 +13,7 @@ namespace SocialMedia
       Get["/"] = _ => {
         return View["index.cshtml"];
       };
-      Post["/login"] = _ => {
+      Post["/login"] = _ => {                                                   //NOTE IM NOT VERY RESTFUL
         Dictionary <string, object> model = new Dictionary<string, object>{};
 
         string username = Request.Form["username"];
@@ -40,7 +40,7 @@ namespace SocialMedia
           }
         }
       };
-      Post["/account_created"] = _ => {
+      Post["/account_created"] = _ => {                                       //NOTE IM NOT VERY RESTFUL
         Dictionary <string, object> model = new Dictionary<string, object>{};
         string firstName = Request.Form["first-name"];
         string lastName = Request.Form["last-name"];
@@ -70,6 +70,27 @@ namespace SocialMedia
             return View["news.cshtml", model];
           }
         }
+      };
+      // Post["/users/{userId}/posts/{postId}/comment"] = parameters => {
+      //   Dictionary <string, object> model = new Dictionary<string, object>{};
+      //   User selectedUser = User.Find(parameters.userId);
+      //   Post selectedPost = Post.Find(parameters.postId);                       //NOTE UHOH, IM A BIG PROBLEM
+      //   Comment newComment = new Comment(Request.Form["comment"], selectedPost.Id, selectedUser.Id, DateTime.Now);
+      //   newComment.Save();
+      //   List<Post> timeline = selectedUser.GetTimeline();
+      //   model.Add("timeline", timeline);
+      //   model.Add("user", selectedUser);
+      //   return View["news.cshtml", model];
+      // };
+      Post["/users/{userId}/post"] = parameters => {
+        Dictionary <string, object> model = new Dictionary<string, object>{};
+        User selectedUser = User.Find(parameters.userId);
+        Post newPost = new Post(Request.Form["post"], selectedUser.Id, DateTime.Now);
+        newPost.Save();
+        List<Post> timeline = selectedUser.GetTimeline();
+        model.Add("timeline", timeline);
+        model.Add("user", selectedUser);
+        return View["news.cshtml", model];
       };
     }
   }
